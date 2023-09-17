e3993b0861c2cb9140b83e368dbe15b0899f288d
golnaaz <golnaz.aliakbar@gmail.com>
2023-09-17 16:11:37 +0200

    Implement DetailViewModel

--- app/src/main/java/com/golnaz/home/ui/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/golnaz/home/ui/viewmodel/DetailViewModel.kt
@@ -1,6 +1,76 @@
 package com.golnaz.home.ui.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.golnaz.home.domain.MainRepository
+import com.golnaz.home.network.model.NetworkResult
+import com.golnaz.home.ui.model.ItemUiModel
+import com.golnaz.home.ui.toContent
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 
-class DetailViewModel: ViewModel() {
+class DetailViewModel(
+    private val repository: MainRepository
+) : ViewModel() {
+    private val eventChannel = Channel<DetailEvent>(Channel.BUFFERED)
+    private val stateFlow = MutableStateFlow(DetailViewState())
+
+    val eventFlow = eventChannel.receiveAsFlow()
+    val state: StateFlow<DetailViewState> = stateFlow
+
+    fun processAction(action: Action) {
+        when (action) {
+            is Action.Init -> {
+                initDetails(action.id)
+            }
+
+            Action.OnBackPressed -> {
+                eventChannel.trySend(DetailEvent.OnPopBackStack)
+            }
+        }
+    }
+
+    private fun initDetails(id: String) {
+        viewModelScope.launch {
+            repository.getDetailItem(id).collect { result ->
+                when (result) {
+                    is NetworkResult.Fail -> {
+                        stateFlow.update {
+                            it.copy(
+                                error = result.error
+                            )
+                        }
+                    }
+
+                    is NetworkResult.Success -> {
+                        stateFlow.update {
+                            it.copy(
+                                userDetail = result.value.toContent(),
+                                error = "",
+                            )
+                        }
+                    }
+                }
+            }
+
+        }
+    }
+
+    sealed class Action {
+        data class Init(val id: String) : Action()
+        object OnBackPressed : Action()
+    }
+
+    sealed class DetailEvent {
+        object OnPopBackStack : DetailEvent()
+    }
+
+    data class DetailViewState(
+        val userDetail: ItemUiModel? = null,
+        val error: String = "",
+    )
 }
