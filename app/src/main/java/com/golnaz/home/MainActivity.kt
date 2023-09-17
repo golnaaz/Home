6ea1671f31301099f7ec09a77f26e163b3960fa7
golnaaz <golnaz.aliakbar@gmail.com>
2023-09-17 16:24:18 +0200

    Add Navigation to MainActivity

--- app/src/main/java/com/golnaz/home/MainActivity.kt
+++ app/src/main/java/com/golnaz/home/MainActivity.kt
@@ -6,10 +6,10 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import com.golnaz.home.navigation.NavGraph
 import com.golnaz.home.ui.theme.HomeTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +22,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Screens()
                 }
             }
         }
@@ -30,17 +30,7 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    HomeTheme {
-        Greeting("Android")
-    }
+private fun Screens() {
+    val navController = rememberNavController()
+    NavGraph(navController)
 }
--- app/src/main/java/com/golnaz/home/di/ViewModelModule.kt
+++ app/src/main/java/com/golnaz/home/di/ViewModelModule.kt
@@ -1,10 +1,11 @@
 package com.golnaz.home.di
 
-
+import com.golnaz.home.ui.viewmodel.DetailViewModel
+import com.golnaz.home.ui.viewmodel.MainViewModel
 import org.koin.androidx.viewmodel.dsl.viewModelOf
 import org.koin.dsl.module
 
 val viewModelModule = module {
     viewModelOf(::MainViewModel)
-    viewModelOf(::DetailsViewModel)
+    viewModelOf(::DetailViewModel)
 }
