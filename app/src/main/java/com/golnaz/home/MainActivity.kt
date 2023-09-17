4f41be9ba7ebdc7521bd89e2cf0aba67cc13fe4e
golnaaz <golnaz.aliakbar@gmail.com>
2023-09-17 15:47:41 +0200

    Fix wrong annotation in MainAc

--- app/src/main/java/com/golnaz/home/MainActivity.kt
+++ app/src/main/java/com/golnaz/home/MainActivity.kt
@@ -29,7 +29,7 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composablelibs.versions.toml
+@Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Hello $name!",
